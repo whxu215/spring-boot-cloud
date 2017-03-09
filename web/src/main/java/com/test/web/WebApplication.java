91ee243a594e28a838a132189885ed3cc4307341
Andy Xu <whxu215@aliyun.com>
2017-03-09 16:39:19 +0800

    Update WebApplication.java
    add @LoadBalanced @EnableEurekaClient annotations

--- web/src/main/java/com/test/web/WebApplication.java
+++ web/src/main/java/com/test/web/WebApplication.java
@@ -1,22 +1,42 @@
 package com.test.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableHystrix
-@ComponentScan
+@ComponentScan(basePackages = {"com.test"})
+@Configuration
+@EnableAutoConfiguration
 @EnableFeignClients
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
-
+    
+    @Autowired  
+    private RestTemplateBuilder builder;  
+  
+    // 使用RestTemplateBuilder来实例化RestTemplate对象，spring默认已经注入了RestTemplateBuilder实例  
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {  
+        return builder.build();  
+    } 
 }
